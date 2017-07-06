c7c2d30d6a62dd50f44c9abf5d1f48139a86bc1f
David Ryan <davidjryan2000@gmail.com>
2017-07-06 10:21:20 +0100

    static images/video upload

--- Products/models.py
+++ Products/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     image = models.ImageField(upload_to='images')
     tag = models.CharField(max_length=20, default='')
 
